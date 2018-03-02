8b7ec6e3e02c631c8c5f87ea66b1886b10402645
nicolaiw@ <nicolaiw@>
2018-03-02 10:04:00 +0100

    comments; README.md

--- src/EpdConverter.Core/EpdImport/IEpdImport.cs
+++ src/EpdConverter.Core/EpdImport/IEpdImport.cs
@@ -8,8 +8,8 @@ namespace EpdConverter.Core.EpdImport
         /// <summary>
         /// Imports an EPD dataset from a given path.
         /// </summary>
-        /// <param name="path">A Filesystem path, an Url or some other path.</param>
-        /// <returns>List of EPD's wheres an EPD corresponds to an indicator with all its modules.</returns>
+        /// <param name="path">A filesystem path, an Url or some other path.</param>
+        /// <returns>List of EPD's whereas an EPD corresponds to an indicator with all its modules.</returns>
         IEnumerable<Epd> GetEpd(string path);
     }
 }
